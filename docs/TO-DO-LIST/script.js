b266d4f5390820f61851128e1c14fb6fc3f3e612
AyushSharma012 <ayushsharma020506@gmail.com>
2025-04-06 01:37:14 +0530

    Add files via upload

---
+++ docs/TO-DO-LIST/script.js
@@ -0,0 +1,26 @@
+document.getElementById("btn").addEventListener("click", addToList);
+
+document.getElementById("task").addEventListener("keypress", function(event) {
+    if (event.key === "Enter") {
+        addToList();
+    }
+});
+
+
+function addToList() {
+    let feild = document.getElementById("task").value; 
+
+    let cells = document.querySelector(".tasks")
+
+    if(feild.length != 0){
+        cells.innerHTML = '<div class = "cell"> <p>' + feild +'</p> <button onclick ="removeFromList()">del</button> </div> ' + cells.innerHTML;
+        document.getElementById("task").value = "";
+    }else{
+        alert("Insert the value first")
+    }
+}
+
+function removeFromList() {
+    document.querySelector(".cell").remove();
+}
+
